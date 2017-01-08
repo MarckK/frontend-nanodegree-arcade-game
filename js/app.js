9a4d468c5adc03a18c4013630ecb546480009df3
Kara de la Marck <karadelamarck@gmail.com>
2017-01-08 15:53:43 +0000

    renders 2 enemies and the canvas :)

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +16,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = (this.x + 1) * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -30,6 +33,10 @@ Enemy.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var enemy1 = new Enemy(50, 50);
+var enemy2 = new Enemy (50, 70);
+
+var allEnemies = [enemy1, enemy2];
 
 
 // This listens for key presses and sends the keys to your
